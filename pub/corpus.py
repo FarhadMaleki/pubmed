87fd5b3c693199b633158116206704a9a4b669d3
Farhad Maleki <farhad.maleki@usask.ca>
2019-09-13 23:12:33 -0400

    Implement and test Corpus class

---
+++ pub/corpus.py
@@ -0,0 +1,43 @@
+'''Extract article information from PubMed baseline gz (gzipped) files.
+
+'''
+import gzip
+from lxml import etree
+
+class Corpus:
+    '''Extract article information for all records in a PubMed baseline file.
+
+    Args:
+         input_address (str): Address of a PubMed baseline gz (gzipped) file.
+         config (dict): A dictionary that determines the information
+            being extracted.
+    '''
+    def __init__(self, input_address, config):
+        self.config = config
+        self.articles = Corpus.collect_articles(input_address, self.config)
+
+    @staticmethod
+    def collect_articles(address, config):
+        '''Extract article information for all records in a PubMed baseline file.
+
+        Args:
+            address: Address of a PubMed baseline gz (gzipped) file.
+
+        Returns:
+            list: A list of data items for each article in the PubMed baseline
+                gz (gzipped) file. The order of items is defined by the order of content
+                in the config object.
+        '''
+        with gzip.open(address, mode='rt', encoding=config['encoding']) as fin:
+            root = etree.parse(fin)
+        etree.strip_tags(root, config["striped_tags"])
+        elements = root.xpath(config['articles_path'])
+        articles = []
+        for element in elements:
+            pmid = element.findtext('PMID')
+            article = element.find(config['article_tag'])
+            info = [pmid]
+            for tag in config['contents']:
+                info.append(article.findtext(tag))
+            articles.append(info)
+        return articles
---
+++ test/test_corpus.py
@@ -0,0 +1,24 @@
+'''This module contains unittests for Corpus class.
+
+'''
+import json
+import unittest
+from pub.corpus import Corpus
+
+
+class TestCorpus(unittest.TestCase):
+    def setUp(self):
+        config_address = 'test/data/config.json'
+        self.corpus_file_address = 'test/data/PubMedSampleFile.xml.gz'
+        articles_file_address = 'test/data/PubMedSampleFile.csv'
+        with open(config_address, 'r') as fin:
+            self.config = json.load(fin)
+        self.corpus_articles = []
+        with open(articles_file_address, 'r', encoding='utf-8') as fin:
+            for line in fin:
+                article = line.strip().split('\t')
+                self.corpus_articles.append(article)
+
+    def test_collect_articles(self):
+        corpus = Corpus(self.corpus_file_address, self.config)
+        self.assertListEqual(corpus.articles, self.corpus_articles)
